2f1ae27fa21a6290f8a1f9f5cee96c89143f40ea
taibuharoub <haroubtaibu@gmail.com>
2021-03-09 19:37:46 +0300

    Bootcamp Routes

--- server.js
+++ server.js
@@ -7,6 +7,43 @@ dotenv.config({ path: "./config/config.env" });
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+app.get("/api/v1/bootcamps", (req, res, next) => {
+  res.status(200).json({
+    success: true,
+    msg: "Show all Bootcamps",
+  });
+});
+
+app.get("/api/v1/bootcamps/:id", (req, res, next) => {
+    res.status(200).json({
+      success: true,
+      msg: `Show Bootcamp ${req.params.id}`,
+    });
+  });
+
+app.post("/api/v1/bootcamps", (req, res, next) => {
+  res.status(200).json({
+    success: true,
+    msg: "Create new Bootcamps",
+  });
+});
+
+app.put("/api/v1/bootcamps/:id", (req, res, next) => {
+    res.status(200).json({
+      success: true,
+      msg: `Update Bootcamp ${req.params.id}`,
+    });
+  });
+
+app.delete("/api/v1/bootcamps/:id", (req, res, next) => {
+  res.status(200).json({
+    success: true,
+    msg: `Delete Bootcamp ${req.params.id}`,
+  });
+});
+
 app.listen(PORT, () => {
-    console.log(`Server Started in ${process.env.NODE_ENV} mode at http://localhost:${PORT}`);
-})
+  console.log(
+    `Server Started in ${process.env.NODE_ENV} mode at http://localhost:${PORT}`
+  );
+});
