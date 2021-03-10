ce7f836a7d7d2bde11c38fa6f59117e209ca0505
taibuharoub <haroubtaibu@gmail.com>
2021-03-10 15:43:51 +0300

    Bootcamp Basic CRUD

--- server.js
+++ server.js
@@ -1,26 +1,46 @@
 const express = require("express");
 const dotenv = require("dotenv");
-const morgan = require("morgan")
-
-//Route files
-const bootcacampRoutes = require("./routes/bootcamps");
+const morgan = require("morgan");
+const colors = require("colors");
+const connectDB = require("./config/db");
 
 //load env vars
 dotenv.config({ path: "./config/config.env" });
 
+//Connect to DB
+connectDB();
+
+//Route files
+const bootcacampRoutes = require("./routes/bootcamps");
+
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+//Body Parser
+// app.use(express.urlencoded({extended: true}));
+app.use(express.json())
+
+
 //Dev logging middleware
 if (process.env.NODE_ENV === "development") {
-    app.use(morgan("dev"));
+  app.use(morgan("dev"));
 }
 
 //Mount routers
 app.use("/api/v1/bootcamps", bootcacampRoutes);
 
-app.listen(PORT, () => {
+const server = app.listen(PORT, () => {
   console.log(
     `Server Started in ${process.env.NODE_ENV} mode at http://localhost:${PORT}`
+      .yellow.bold
   );
+});
+
+//Hnadle unhandled promise ejections
+process.on("unhandledRejection", (err, promise) => {
+  console.log(`Error: ${err.message}`.red.bold);
+  //Close server & exit process
+  server.close(() => {
+    process.exit(1);
+  });
 });
